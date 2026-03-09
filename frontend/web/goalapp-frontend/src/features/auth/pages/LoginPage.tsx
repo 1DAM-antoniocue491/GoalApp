09b95625c77e3eb79264145a7402d420bbfbaada
Antonio <acuelop369@g.educaand.es>
2026-03-09 18:03:15 +0100

    ç

--- frontend/web/goalapp-frontend/src/features/auth/pages/LoginPage.tsx
+++ frontend/web/goalapp-frontend/src/features/auth/pages/LoginPage.tsx
@@ -1,6 +1,22 @@
+import { Link } from "react-router";
+
 export default function LoginPage() {
     return (
         <>
+        <div className="bg-zinc-950 h-screen flex justify-center items-center">
+            <div className="bg-zinc-900">
+                <p>GoalApp</p>
+                <h2>Bienvenido</h2>
+
+                <div>
+                    <Link to={'/login'}>Iniciar Sesión</Link>
+                    <Link to={'/register'}>Registrarse</Link>
+                </div>
+
+                <form action=""></form>
+
+            </div>
+        </div>
         </>
     )
 }
